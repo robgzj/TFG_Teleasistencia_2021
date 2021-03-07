4d0a03e71a0a481ddb6e45c404ecb7e142a475fc
Roberto González Jiménez <rgj530@alumnos.unican.es>
2021-03-07 19:44:59 +0100

    Añadida la tabla de mostrar los datos de los sensores y actualizado el diario y las anotaciones

--- AndroidProject/app/src/main/java/com/example/tfg_teleasistencia_2021/MainActivity.java
+++ AndroidProject/app/src/main/java/com/example/tfg_teleasistencia_2021/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.tfg_teleasistencia_2021;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -16,6 +19,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity{
 
     Button btn_acercaDe, btn_datosSensores;
+    Switch switchB;
 
 
     @Override
@@ -28,6 +32,28 @@ public class MainActivity extends AppCompatActivity{
 
         btn_datosSensores = findViewById(R.id.datos_sensores);
         btn_datosSensores.setOnClickListener(v -> openDatosActivity());
+
+        //Guardar estado switch
+        switchB=findViewById(R.id.encender_app);
+        SharedPreferences sharedPreferences= getSharedPreferences("save",MODE_PRIVATE);
+        switchB.setChecked(sharedPreferences.getBoolean("value", false));
+
+        switchB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(switchB.isChecked()){
+                    SharedPreferences.Editor editor=getSharedPreferences("save",MODE_PRIVATE).edit();
+                    editor.putBoolean("value",true);
+                    editor.apply();
+                    switchB.setChecked(true);
+                }else{
+                    SharedPreferences.Editor editor=getSharedPreferences("save",MODE_PRIVATE).edit();
+                    editor.putBoolean("value",false);
+                    editor.apply();
+                    switchB.setChecked(false);
+                }
+            }
+        });
     }
 
     public void openAcercaDeActivity(){
